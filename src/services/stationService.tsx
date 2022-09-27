40a5ba33e04c0663b87ae9e68343f76377db62f8
Rafal Kazimierski <rafal.kazimierski@ttpsc.pl>
2022-09-28 00:10:35 +0200

    Addess ready for access point

--- src/services/stationService.tsx
+++ src/services/stationService.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const baseUrl = 'http://localhost:8000';
+export const baseUrl = 'http://10.0.0.1:8000';
 export const workstation = 'testworkstation';
-export const websocketBaseAddress = 'ws://localhost:8000/';
+export const websocketBaseAddress = 'ws://10.0.0.1:8000/';
 
 export const addTask = async (action: string, target: any, value: number) => {
   console.log({
