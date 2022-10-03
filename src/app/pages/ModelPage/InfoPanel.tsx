ee23f1547d87d108b1d5569084dc1d4f4b734e84
gerwant <damian.gorzelanczyk@gmail.com>
2022-10-03 18:19:17 +0200

    Modal content for sensors' calibration

--- src/app/pages/ModelPage/InfoPanel.tsx
+++ src/app/pages/ModelPage/InfoPanel.tsx
@@ -1,18 +1,41 @@
 import React, { useState } from 'react';
 
 import styled from 'styled-components';
+import axios from 'axios';
 import { Button, Modal, Placeholder } from 'rsuite';
 
 import { BaseInfoPanel } from './BaseInfoPanel';
 import { CalibrationButton } from './CalibrationButton';
+import { baseUrl, workstation } from 'services/stationService';
 
 export const InfoPanel = ({ currentItem, ...props }) => {
   const [isModalOpen, setModalOpen] = useState(false);
   const [currentContainer, setCurrentContainer] = useState('');
+  const [isModalLoading, setModalLoading] = useState(false);
 
   const toggleModal = containerid => {
     setCurrentContainer(containerid);
     setModalOpen(!isModalOpen);
+  };
+
+  const sendCalibrationRequest = e => {
+    setModalLoading(true);
+
+    axios
+      .post(
+        `${baseUrl}/calibrate/${workstation}/${currentContainer}`,
+        {},
+        { withCredentials: true },
+      )
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log('error ', err);
+      })
+      .finally(() => {
+        setModalLoading(false);
+      });
   };
 
   return (
@@ -30,17 +53,50 @@ export const InfoPanel = ({ currentItem, ...props }) => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Placeholder.Paragraph />
+          <p>
+            Żeby skalibrować czujnik w zbiorniku {currentContainer}, wykonaj
+            poniższe kroki:
+          </p>
+          <br />
+          <ol>
+            <li>
+              Opróżnij zbiornik z użyciem{' '}
+              {['C1', 'C5'].includes(currentContainer) ? 'pompy' : 'zaworu'}.
+              <br />
+              W zbiorniku powinna zostać woda zgromadzona do wysokości ok. 1cm
+              ze względu na nakrętkę przy odpływie.
+              <br />
+              Możliwa jest sytuacja cofnięcia się wody z węży do zbiornika.
+              Postaraj się opróżnić zbiornik tak bardzo jak to możliwe.
+            </li>
+            <li>
+              Napełnij zbiornik wodą do wysokości <b>10cm</b>.
+            </li>
+            <li>
+              Upewnij się, że woda jest na poziomie <b>10cm</b> zgodnie z miarką
+              na zbiorniku i kliknij przycisk <i>Gotowe</i>.
+              <br />
+              <b>Uwaga!</b> Kliknięcie przycisku <i>Gotowe</i> oznacza wpisanie
+              nowych ustawień do bazy danych. Jest to operacja nieodwracalna.
+              Powrót do poprzednich ustawień nie jest możliwy.
+            </li>
+          </ol>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={toggleModal} appearance="primary">
-            Ok
+          <Button
+            onClick={sendCalibrationRequest}
+            color="green"
+            appearance="primary"
+            loading={isModalLoading}
+          >
+            Gotowe
           </Button>
           <Button
             onClick={() => {
-              setModalOpen(false);
+              toggleModal(currentContainer);
             }}
-            appearance="subtle"
+            appearance="primary"
+            color="red"
           >
             Anuluj
           </Button>
