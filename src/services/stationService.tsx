65bcbc3a5f9164c956d64f6f3569a8640b92d433
gerwant <damian.gorzelanczyk@gmail.com>
2022-10-22 21:29:25 +0200

    Backend address is variable

--- src/services/stationService.tsx
+++ src/services/stationService.tsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-export const baseUrl = 'http://10.0.0.1:8000';
+export const baseUrl = `http://${window.location.host.split(':')[0]}:8000`;
 export const workstation = 'testworkstation';
-export const websocketBaseAddress = 'ws://10.0.0.1:8000/';
+export const websocketBaseAddress = `ws://${
+  window.location.host.split(':')[0]
+}:8000/`;
 
 export const addTask = async (action: string, target: any, value: number) => {
   console.log({
